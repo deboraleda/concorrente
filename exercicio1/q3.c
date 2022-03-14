1a0eb6d2bb33378b840cb4f4360b8ef6f878397b
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2022-03-14 15:00:42 -0300

    adiciona código

--- exercicio1/q3.c
+++ exercicio1/q3.c
@@ -4,13 +4,13 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+sem_t *mutex;
 sem_t *sem1;
-sem_t *sem2;
 
 struct thread_param {
   long t_id;
   int random_s;
-  int s_prev_thread;
+  int prev_s;
 };
 
 int ended_threads_count = 0;
@@ -25,29 +25,31 @@ void *phase_one(void *t){
     int s = rand() % 10;
     param->random_s = s;
     printf("PHASE ONE: Thread %ld DONE! With random_s = %d\n", id, s);
-    sem_wait(sem2);
+
+    sem_wait(sem1);
     ended_threads_count++;
     if(ended_threads_count == t_number){
-        sem_post(sem1);
+        sem_post(mutex);
     }
-    sem_post(sem2);
+    sem_post(sem1);
+
     pthread_exit(NULL);
 }
 
 void *phase_two(void *t){
-    sem_wait(sem2);
     struct thread_param *param = (struct thread_param*) t;
-    int time_sleep = (int)param->s_prev_thread;
-    printf("PHASE TWO: Thread %ld with s_prev_thread = %d\n", param->t_id, param->s_prev_thread);
-    sem_post(sem2);
+    int time_sleep = (int)param->prev_s;
+    printf("PHASE TWO: Thread %ld with prev_s = %d\n", param->t_id, param->prev_s);
     sleep(time_sleep);
     printf("PHASE TWO: Thread %ld DONE!\n", param->t_id);
-    sem_wait(sem2);
+
+    sem_wait(sem1);
     ended_threads_count++;
     if (ended_threads_count == t_number){
-        sem_post(sem1);
+        sem_post(mutex);
     }
-    sem_post(sem2);
+    sem_post(sem1);
+
     pthread_exit(NULL);
 }
 
@@ -59,8 +61,8 @@ int main(int argc, char *argv[]){
     pthread_t threads[n];
     struct thread_param params[n];
 
-    sem1 = sem_open("/sem1", O_CREAT, 0644, 0);
-    sem2 = sem_open("/sem2", O_CREAT, 0644, n);
+    mutex = sem_open("/mutex", O_CREAT, 0644, 0);
+    sem1 = sem_open("/sem1", O_CREAT, 0644, 1);
 
     printf("-----Phase One-----\n");
 
@@ -69,8 +71,7 @@ int main(int argc, char *argv[]){
         pthread_create(&threads[i], NULL, phase_one, (void *)&params[i]);
     }
 
-    sem_wait(sem1);
-    printf("End of Phase One\n");
+    sem_wait(mutex);
 
     printf("-----Phase Two-----\n");
 
@@ -79,16 +80,13 @@ int main(int argc, char *argv[]){
     for (int i = 0; i < n; i++){
         int prev_thread_id = (i + n - 1) % n;
         int s = params[prev_thread_id].random_s;
-        params[i].s_prev_thread = s;
+        params[i].prev_s = s;
         pthread_create(&threads[i], NULL, phase_two, (void *)&params[i]);
     }
 
-    sem_wait(sem1);
-    printf("End of Phase Two\n");
-
+    sem_close(mutex);
+    sem_unlink("/mutex");
     sem_close(sem1);
     sem_unlink("/sem1");
-    sem_close(sem2);
-    sem_unlink("/sem2");
     pthread_exit(NULL);
 }
