2dbf94ad247231ff0a41883d6bfb237543d5b239
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2022-03-28 16:39:13 -0300

    remove shared memory e add channels

--- prova3/q2.go
+++ prova3/q2.go
@@ -9,49 +9,46 @@ import (
 	"sync"
 )
 
-var array_s = []int{}
-var n *int;
-var mutex = make(chan int, 1)
-
 var barrier sync.WaitGroup
 var wg sync.WaitGroup
 
-func goroutine_func(tid int, barrier *sync.WaitGroup, wg *sync.WaitGroup){
+func goroutine_func(tid int, send_ch chan int, receive_ch chan int){
 	defer wg.Done()
+	//Phase one
 	time_sleep := rand.Intn(5)
 	fmt.Printf("PHASE ONE: Thread %d sleeping for %d seconds\n", tid, time_sleep)
 	time.Sleep(time.Duration(time_sleep))
 	s := rand.Intn(10)
-	<- mutex
-	array_s = append(array_s, s)
-	mutex <- 1
+	send_ch <- s
+	close(send_ch)
 	fmt.Printf("PHASE ONE: Thread %d DONE! With random_s = %d\n", tid, s)
 	barrier.Done()
 	
 	barrier.Wait()
 
-	prev_id := (tid + *n - 1) % *n
-	<- mutex
-	prev_s := array_s[prev_id]
-	mutex <- 1
-
+	//Phase two
+	prev_s := <- receive_ch
 	fmt.Printf("PHASE TWO: Thread %d sleeping for %d seconds\n", tid, prev_s)
 	time.Sleep(time.Duration(prev_s))
 	fmt.Printf("PHASE TWO: Thread %d DONE\n", tid)
 }
 
-func main() {
-	x, _ := strconv.Atoi(os.Args[1])
+func main(){
+	n, _ := strconv.Atoi(os.Args[1])
 	rand.Seed(time.Now().UnixNano())
-	mutex <- 1
 
-	n = &x
-
-	barrier.Add(*n)
-	wg.Add(*n)
-	for i := 0; i < *n; i++ {
-		go goroutine_func(i, &barrier, &wg)
+	ch_array := make([]chan int, n)
+	for i := range ch_array {
+		ch_array[i] = make(chan int, 1)
 	}
 
+	barrier.Add(n)
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		prev_id := (i + n - 1) % n
+		receive_ch := ch_array[prev_id];
+		send_ch := ch_array[i]
+		go goroutine_func(i, send_ch, receive_ch)
+	}
 	wg.Wait()
 }
