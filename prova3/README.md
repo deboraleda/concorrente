27fd7cb28bd1da3e43442960781042233017b32f
deboraleda <debora.souza@ccc.ufcg.edu.br>
2022-03-29 20:17:39 -0300

    adiciona questao 3 da prova 3

--- prova3/README.md
+++ prova3/README.md
@@ -37,3 +37,19 @@ Exemplo de execução:
 <img src="../img/p3q2.png" width="500"/>
 
 ## Questão 3
+
+Compile o código com o seguinte comando:
+
+```shell
+go build q3.go
+```
+
+Em seguida, execute passando o valor de `n` como parâmetro:
+
+```shell
+./q3 dir/full/path
+```
+
+Exemplo de execução usando o diretório do repositório `dirteste`:
+
+<img src="../img/p3q3.png" width="600"/>
---
+++ prova3/q3.go
@@ -0,0 +1,56 @@
+
+
+package main
+ 
+import (
+   "os"
+   "fmt"
+	"sync"
+   "path/filepath"
+)
+
+var wg sync.WaitGroup
+
+
+func walkDir(dir string, ch chan string){
+
+   filepath.Walk(dir, func(path string, info os.FileInfo, err error) error{
+		if !info.IsDir() {
+         ch <- path
+		}
+      return nil
+	})
+   
+   close(ch)
+
+   wg.Done()
+}
+
+
+func readFile(ch chan string){
+   defer wg.Done()
+   for file := range ch {
+      f, _ := os.Open(file)
+
+      b1 := make([]byte, 1)
+      f.Read(b1)
+      
+
+      if (b1[0] % 2) == 0 {
+         fmt.Println("File", file)
+      }
+	}   
+}
+ 
+
+func main() {
+   dir := os.Args[1]
+
+   ch := make(chan string)
+
+   wg.Add(2)
+   go walkDir(dir, ch)
+   go readFile(ch)
+
+   wg.Wait()	 
+}
